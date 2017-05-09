e0e39656151bc3d1c367dac15e489b6460b51ed3
Fede Godoy <fede.godoy1@gmail.com>
2017-05-09 10:09:14 -0300

    Proyecto en Maven

---
+++ Bootcamp2017_Java/src/main/java/main/Day.java
@@ -0,0 +1,90 @@
+package main;
+
+/**
+ *
+ * @author federico
+ */
+public class Day{
+    private String name;
+    private String date;
+    private String description;
+    private Location location;
+    private Atmosphere atmosphere;
+    private Wind wind;
+    private Temperature temp;
+
+    public Day(String name, String date, String description, Location location, Atmosphere atmosphere, Wind wind, Temperature temp) {
+        this.name = name;
+        this.date = date;
+        this.description = description;
+        this.location = location;
+        this.atmosphere = atmosphere;
+        this.wind = wind;
+        this.temp = temp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public Atmosphere getAtmosphere() {
+        return atmosphere;
+    }
+
+    public void setAtmosphere(Atmosphere atmosphere) {
+        this.atmosphere = atmosphere;
+    }
+
+    public Wind getWind() {
+        return wind;
+    }
+
+    public void setWind(Wind wind) {
+        this.wind = wind;
+    }
+
+    public Temperature getTemp() {
+        return temp;
+    }
+
+    public void setTemp(Temperature temp) {
+        this.temp = temp;
+    }
+
+    @Override
+    public String toString() {
+        return "\nDay-----------------\n" + 
+                "Name: " + name + ", date: " + date + ", description: " + description + ", " + location.toString() + ", " + atmosphere.toString() + ", " + wind.toString() + ", " + temp.toString();
+    }
+    
+    
+    
+}
---
+++ Bootcamp2017_Java/src/main/java/main/Node.java
@@ -0,0 +1,46 @@
+package main;
+
+/**
+ *
+ * @author federico
+ */
+public class Node {
+    private Object info;
+    private Node next;
+   
+    public Node ( )
+    {
+    }
+
+    public Node (Object x, Node p)
+    {
+     info = x;
+     next = p;
+    }
+
+    public Node getNext()
+    {
+     return next;
+    }
+
+    public void setNext(Node p)
+    {
+     next = p;
+    }
+
+    public Object getInfo()
+    {
+     return info;
+    }
+
+    public void setInfo(Object p)
+    {
+     if( p != null ) info = p;
+    }
+
+    @Override
+    public String toString()
+    {
+     return info.toString();   
+    }
+}
